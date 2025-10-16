dd13688d122ba16a755b4c84c8f1f90f87bc8a68
Ivan <ivan06owi@gmail.com>
2025-10-16 11:56:29 +0800

    vvvvv

--- client/src/pages/Games.jsx
+++ client/src/pages/Games.jsx
@@ -123,7 +123,7 @@ export default function Games() {
               filter: "drop-shadow(0 0 6px #00f0ff)",
             }}
           >
-            GG Review
+            GG Reviewer
           </Typography>
           <Typography
             sx={{
@@ -145,7 +145,7 @@ export default function Games() {
               },
             }}
           >
-            GG Review
+            GG Reviewer
           </Typography>
         </Box>
 
